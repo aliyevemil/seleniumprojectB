b9af64a15b50499e8d6794d3538ca4880cdb3579
Emil Aliyev <aliyevemil1991@hotmail.com>
2022-09-04 14:25:39 +0300

    selenium

--- test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -1,13 +1,16 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DropdownPractices {
 
     @Test
-    public void dropdown_task5(){
+    public void dropdown_task5() throws InterruptedException {
 
         //TC #5: Selecting state from State dropdown and verifying result
         //1. Open Chrome browser
@@ -16,10 +19,23 @@ public class DropdownPractices {
 
         //2. Go to http://practice.cybertekschool.com/dropdown
         driver.get("http://practice.cybertekschool.com/dropdown");
+
+        Select stateDropdown =new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
         //3. Select Illinois
+        Thread.sleep(1000);
+        stateDropdown.selectByVisibleText("Illinois");
         //4. Select Virginia
+        Thread.sleep(1000);
+        stateDropdown.selectByValue("VA");
         //5. Select California
+        Thread.sleep(1000);
+        stateDropdown.selectByIndex(5);
         //6. Verify final selected option is California.
+        String expectedOptionText="California";
+        String actualOptionText=stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText,expectedOptionText);
         //Use all Select options. (visible text, value, index)
     }
 }
