bd33804980e90da8072461d68eddeae190930958
Emil Aliyev <aliyevemil1991@hotmail.com>
2022-09-05 19:13:42 +0300

    day-06 Alert Practice 1

--- test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -35,12 +37,45 @@ public class T5_Windows_Practice {
 
         //4. Assert: Title is “Windows”
         String expectedTitle ="Windows";
-        String actualTitle = "Windows";
+        String actualTitle = driver.getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
+
+        System.out.println("Title before click: "+actualTitle);
+
         //5. Click to: “Click Here” link
+        WebElement clickHereLink= driver.findElement(By.linkText("Click Here"));
+
+        clickHereLink.click();
+
+
+
         //6. Switch to new Window.
+        for (String each : driver.getWindowHandles()){
+
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: "+driver.getTitle());
+
+
+        }
+
+
+
+
+
         //7. Assert: Title is “New Window”
+        String expectedTitleAfterClick="New Window";
+        actualTitle= driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitleAfterClick);
+        System.out.println("Title after click: "+ actualTitle);
+
+
+
+
+        actualTitle= driver.getTitle();
+        System.out.println("Title after click: "+actualTitle);
+
     }
 
 }
